542541d526352ceb2328a82587dd16feb0a47f76
Mitch4sho <mitchelloliver1991@gmail.com>
2020-12-16 05:25:56 -0800

    learn how to assigned variables to and how to print values

---
+++ HelloWorld/app.py
@@ -0,0 +1,16 @@
+# age = 20
+# price = 19.95
+# first_name  = "Mitchell"
+# is_online = False
+# print(age)
+
+name = input ("what is your name? " )
+print ("Hello " + name)
+
+# class Person:
+#   def __init__(self, age, status):
+#     self.age = age
+#     self.status = status
+
+# p1 = Person("mitchell", 29)
+# print(p1.age)
